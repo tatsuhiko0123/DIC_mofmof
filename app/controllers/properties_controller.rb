b86b263a932b8c59616b5d6391b4a51468301f13
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-05-09 21:26:28 +0900

    viewの編集

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,8 +1,19 @@
 class PropertiesController < ApplicationController
   def index
+    @property = Property.all
   end
 
   def new
+    @property = Property.new
+  end
+
+  def create
+    Property.create(name: params[:property][:name], 
+                    rent: params[:property][:rent],
+                    address: params[:property][:address], 
+                    age_building: params[:property][:age_building], 
+                    remarks: params[:property][:remarksg])
+    redirect_to new_property_path
   end
 
   def show
