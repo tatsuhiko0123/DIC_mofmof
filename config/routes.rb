2e99fe57ca1089568b7e4158d369c99b1eef3f5d
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-05-09 17:32:17 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'stations/new'
-  get 'stations/show'
-  get 'stations/edit'
-  get 'properties/index'
-  get 'properties/new'
-  get 'properties/show'
-  get 'properties/edit'
-  get 'properties/confirm'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'properties#index'
+  resources :properties
+  resources :stations
 end
