d6eb0d5589189f64ef0eb5015de96f760b39e61a
tatsuhiko0123 <tatsuhiko.t.123@gmail.com>
2021-05-12 13:37:40 +0900

    バリデーションの追加

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,5 @@
 class Property < ApplicationRecord
+  validates :name, presence: true
   has_many :stations, dependent: :destroy, inverse_of: :property
   accepts_nested_attributes_for :stations, allow_destroy: true, reject_if: :all_blank
 end
